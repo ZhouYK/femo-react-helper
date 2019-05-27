e40aa9d907c16f101f22efb7ac8aca91e57f3460
zhouyk <zhouyunkui@bytedance.com>
2019-05-27 19:55:52 +0800

    init

---
+++ readme.md
@@ -0,0 +1,3 @@
+## femo-react-helper
+
+Help "femo" inject data props inio react components, like react-redux
---
+++ src/index.jsx
@@ -0,0 +1,35 @@
+
+
+const rect = (femo) => (deps, callback) => (Component) => {
+  class FemoComponent extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {};
+      this.callback = (...args) => {
+        const state = callback(...args);
+        if (Object.prototype.toString.call(state) === '[object Object]') {
+          this.setState({
+            ...state
+          });
+        } else if (state === undefined) {
+          console.error('you returned "undefined" from callback');
+        } else {
+          console.error('callback should return plain object data');
+        }
+      };
+      this.unsubscribe = femo.subscribe(deps, this.callback);
+    }
+
+    componentWillUnmount() {
+      this.unsubscribe();
+    }
+
+    render() {
+      return <Component { ...this.props } { ...this.state } />
+    }
+  }
+  FemoComponent.displayName = Component.displayName || Component.name;
+  return FemoComponent;
+};
+
+export default rect;
